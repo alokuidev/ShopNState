fb06152f7ba47c115bd594f8c927beeefc37455d
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-03-19 23:03:45 +0530

    remove product implemented

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addCart } from "../Redux/TodoSlice";
+import { addCart, removeCart } from "../Redux/TodoSlice";
 
 const Cart = () => {
   const addedProduct = useSelector((state) => state.cartList);
   const dispatch = useDispatch();
   useEffect(() => {
-    console.log(JSON.stringify(addedProduct));
+   // console.log(JSON.stringify(addedProduct));
   }, [addedProduct]);
   return (
     <>
@@ -17,9 +17,9 @@ const Cart = () => {
             <div className="cart-item" key={elem.id}>
               <p className="cart-item-name">{elem.name.length > 7 ? elem.name.slice(0,8)+'...': elem.name}</p>
               <div className="cart-quantity">
-                <button className="qty-btn">-</button>
+                <button className="qty-btn" onClick={() =>dispatch(removeCart({id:elem.id}))}>-</button>
                 <span className="qty-number">{elem.quantity}</span>
-                <button className="qty-btn" onClick={() =>dispatch(addCart({id:elem.id,name:elem.title,price:elem.price}))}>+</button>
+                <button className="qty-btn" onClick={() =>dispatch(addCart({id:elem.id}))}>+</button>
               </div>
               <p className="cart-item-price">${elem.price}</p>
             </div>
--- src/Redux/TodoSlice.js
+++ src/Redux/TodoSlice.js
@@ -11,16 +11,28 @@ const TodoSlice = createSlice({
             const itemIndex = state.findIndex((item) => item.id === action.payload.id);
             if (itemIndex !== -1) {
                 // Product exists, increase quantity
+                console.log(JSON.stringify(state));
                 state[itemIndex].quantity += 1;
               } else {
                 // New product, set quantity to 1
                 state.push({ ...action.payload, quantity: 1 });
               }
               //console.log(JSON.parse(JSON.stringify(state)));
-        } 
+        },
+        removeCart:(state,action) => { 
+            const itemIndex = state.findIndex((item) => item.id === action.payload.id);
+            if(itemIndex != -1){
+              state[itemIndex].quantity -=1;
+
+              if(state[itemIndex].quantity === 0){
+                state.splice(itemIndex, 1);
+              }
+            }
+        }
+
     }
 })
 
-export const {addCart} = TodoSlice.actions;
+export const {addCart, removeCart} = TodoSlice.actions;
 
 export default TodoSlice.reducer;
